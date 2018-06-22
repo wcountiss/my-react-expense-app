0adc46c977cb450093982237cbacc6082cf7d140
William <william@Williams-MacBook-Pro.local>
2018-06-22 07:11:46 -0400

    hardcoded firebase config for heroku

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,21 @@
 import * as firebase from 'firebase';
 
+// const config = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.FIREBASE_DATABASE_URL,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+// };
+
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: "AIzaSyCkNneO0u4asIIqeqCcRPDuJ13BBbPEDSc",
+  authDomain: "myexpenseproject.firebaseapp.com",
+  databaseURL: "https://myexpenseproject.firebaseio.com",
+  projectId: "myexpenseproject",
+  storageBucket: "myexpenseproject.appspot.com",
+  messagingSenderId: "1064073006835"
 };
 
 firebase.initializeApp(config);
